7f5402e7a13ee740299deecdedda44dedb95390e
hector rojas <hectorucv26@gmail.com>
2024-08-31 15:07:43 -0400

    modificacion

--- assests/js/main.js
+++ assests/js/main.js
@@ -150,7 +150,7 @@ let opt = {
   filename:     'CV_HECTOR_ROJAS.pdf',
   image:        { type: 'jpeg', quality: 0.98 },
   html2canvas:  { scale: 4 },
-  jsPDF:        {  format: 'a4', orientation: 'portrait' }
+  jsPDF:        {  format: 'a3', orientation: 'landscape' }
 };
 
 
